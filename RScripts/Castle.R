643eff991e5fb48f96a6a63bafe85df807c7aafa
lindaverse <linda.hutcheson@gmail.com>
2014-08-24 14:50:44 +0100

    Updated R Script to contain function to find best match for AKA names.

--- RScripts/Castle.R
+++ RScripts/Castle.R
@@ -25,11 +25,24 @@ normalise_col <- function(column) {
     gsub("[^a-z]", "", lowercased)
 }
 
+#Function that selects best key from a name with an aka.
+selectBestKey <- function(name) {
+  if (!grepl("aka", name)) {name}
+  else {
+    if (gsub("aka.*", "", name) %in% genders$characterIndex) {(gsub("aka.*", "", name))}
+    else if (gsub(".*aka", "", name) %in% genders$characterIndex) {(gsub(".*aka", "", name))}
+    else {name}
+  }
+}
+
 #Normalise data that will be used to merge tables.
 genders$characterIndex <-normalise_col(genders$characterName)
 genders$episodeIndex <- normalise_col(genders$episodeName)
 roles$characterIndex <- normalise_col(roles$name)
 roles$episodeIndex <- normalise_col(roles$episodeName)
+
+#Select best key for names containing aka.
+roles$characterIndex <- mapply(roles$characterIndex, FUN=selectBestKey)
 
 #Merge data on character name and episode name.
 gendersWithRoles <- merge(roles, genders, by = c("characterIndex", "episodeIndex"), all.x = T)
@@ -38,4 +51,6 @@ gendersWithRoles <- merge(roles, genders, by = c("characterIndex", "episodeIndex
 toKeep <- c("name", "role", "season", "episodeName.x", "actorName", "gender")
 gendersWithRoles <- gendersWithRoles[, c("name", "role", "season", "episodeName.x", "actorName", "gender"), with = FALSE]
 gendersWithRoles <- rename(gendersWithRoles, c("episodeName.x" = "episodeName"))
+
+#Write CSV file to current working directory. Entries that couldn't be merged contain NA values.
 write.table(gendersWithRoles, "./Data/gendersWithRoles.CSV", sep = ",", row.names =T)
